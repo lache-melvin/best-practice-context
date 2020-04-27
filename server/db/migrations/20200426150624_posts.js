cad2fc646149ece0427a66ce1e33bedddda2964b
Don Smith <don@antiantonym.com>
2020-04-27 18:27:02 +1200

    fix order of migrations

--- server/db/migrations/20200426150624_posts.js
+++ server/db/migrations/20200426150624_posts.js
@@ -4,6 +4,7 @@ exports.up = (knex) => {
     table.string('name')
     table.string('link')
     table.string('description')
+    table.integer('author_id').references('users.id')
     table.datetime('created').defaultTo(knex.fn.now())
     table.datetime('updated').defaultTo(knex.fn.now())
   })
