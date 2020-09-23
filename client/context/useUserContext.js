38bae56ee447f378acc6a55acdaa3f988c15e6fa
Lache Melvin <lache.melvin@gmail.com>
2020-09-24 11:57:30 +1200

    fix token to userState bug

--- client/context/useUserContext.js
+++ client/context/useUserContext.js
@@ -6,7 +6,7 @@ const useUserContext = () => {
   const [user, setUser] = useContext(UserContext);
 
   const applyUser = (token) => {
-    const { id, username } = token;
+    const { id, username } = token.dataValues;
     const user = { id, username };
     setUser(user);
   };
