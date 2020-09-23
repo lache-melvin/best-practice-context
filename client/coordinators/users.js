49705dde268bb42a77ddbef01055262d9a4e697e
Lache Melvin <lache.melvin@gmail.com>
2020-09-23 20:05:41 +1200

    add catch blocks to users coordinators

--- client/coordinators/users.js
+++ client/coordinators/users.js
@@ -1,23 +1,33 @@
 export function makeRegisterUser(configuredRegister, isAuthenticated) {
   return function (credentials, history, signedIn) {
-    return configuredRegister(credentials).then((token) => {
-      if (isAuthenticated()) {
-        signedIn(token);
-        history.push("/");
-      }
-      return null;
-    });
+    return configuredRegister(credentials)
+      .then((token) => {
+        if (isAuthenticated()) {
+          signedIn(token);
+          history.push("/");
+          return;
+        }
+        throw new Error("authentication failure");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 }
 
 export function makeSignInUser(configuredSignIn, isAuthenticated) {
   return function (credentials, history, signedIn) {
-    return configuredSignIn(credentials).then((token) => {
-      if (isAuthenticated()) {
-        signedIn(token);
-        history.push("/");
-      }
-      return null;
-    });
+    return configuredSignIn(credentials)
+      .then((token) => {
+        if (isAuthenticated()) {
+          signedIn(token);
+          history.push("/");
+          return;
+        }
+        throw new Error("authentication failure");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 }
