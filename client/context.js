67a43bd197049badf35d5b32196dfbb489fbbd64
Lache Melvin <lache.melvin@gmail.com>
2020-09-27 19:37:44 +1300

    add default entry context value to fix failing tests 🙈

--- client/context.js
+++ client/context.js
@@ -1,7 +1,9 @@
 import { newContext, createCombinedProvider } from "./context-utils";
 
 export const EntriesContext = newContext("Entries", []);
-export const EntryContext = newContext("Entry", {});
+// using defaultValue as wrapped EntryDetail is used in Entries component test
+// TODO: figure out if this is the best way...
+export const EntryContext = newContext("Entry", {}, [[], () => []]);
 export const UserContext = newContext("User", {});
 
 export const ContextProvider = createCombinedProvider(
