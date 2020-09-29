e7a5d380007b72d29b1a2e20b3feccf2729d8eea
Lache Melvin <lache.melvin@gmail.com>
2020-09-29 21:14:32 +1300

    add enzyme config directly to test file
    if enzyme ends up in frontend deps we can put the setup filepath in the jest config there, and then use the enzyme setup file

--- client/wrappers/entriesContext.test.js
+++ client/wrappers/entriesContext.test.js
@@ -1,11 +1,10 @@
-// NOTE: You'll need to add the following lines to the "jest" config in the
-// eda-frontend-deps package.json:
-// "setupFiles": [
-//   "./testing/enzyme-setup.js"
-// ]
-
 import React from "react";
-import { mount } from "enzyme";
+import enzyme, { mount } from "enzyme";
+
+import Adapter from "enzyme-adapter-react-16";
+enzyme.configure({
+  adapter: new Adapter(),
+});
 
 import makeEntriesContextWrapper from "./entriesContext";
 import { EntriesContext } from "../context";
